1b9d504f1ce7c06d5076fa0512f0708b6cdda1c8
DanielS <codesurfer@gmail.com>
2019-08-08 11:12:33 -0400

    First commit

---
+++ EIDSSAVR/bv.winclient/BasePanel/IMainForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+
+namespace bv.winclient.BasePanel
+{
+    public interface IMainForm
+    {
+        string Text { get; set; }
+
+        void AddButtons();
+        void CallLogin();
+        void InitAutoLogoutMonitor();
+        void RegisterActions();
+        void RegisterDefaultActions();
+        void ResetLanguage(string langID);
+        void SetRussianLanguage();
+        void ShowEventDetail(object eventId);
+        void RefreshLayout();    
+    }
+}
